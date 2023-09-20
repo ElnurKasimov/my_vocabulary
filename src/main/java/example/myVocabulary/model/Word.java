a3c982e432eaa69cc4f4b948b82f4ea373de04f0
Elnur Kasimov <ekasimov2012@gmail.com>
2023-09-20 16:35:26 +0300

    refactored header.html and main.css

--- src/main/java/example/myVocabulary/model/Word.java
+++ src/main/java/example/myVocabulary/model/Word.java
@@ -1,6 +1,7 @@
 package example.myVocabulary.model;
 
 import jakarta.persistence.*;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
