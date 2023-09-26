e11ad8abc5f1fd760a4f31106c5381a6f84f615d
Elnur Kasimov <ekasimov2012@gmail.com>
2023-09-26 14:57:24 +0300

    added implementation of custom methods in WordRepository

--- src/main/java/example/myVocabulary/repository/WordRepository.java
+++ src/main/java/example/myVocabulary/repository/WordRepository.java
@@ -2,9 +2,14 @@ package example.myVocabulary.repository;
 
 import example.myVocabulary.model.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface WordRepository extends JpaRepository<Word, Long> {
     public Word findByForeignWord(String foreignWord);
-    public Word findByTranslation(String translation);
-    public Word findByWordPart(String wordPart);
+    public Word findByTranslationWord(String translation);
+    @Query("SELECT w FROM Word w WHERE LOWER(w.foreignWord) LIKE %:wordPart% OR LOWER(w.translationWord) LIKE %:wordPart%")
+    public List<Word> findByWordPart(@Param("wordPart") String wordPart);
 }
