f4da0519b998c2da92311b96fe655928b8c07bdb
Elnur Kasimov <ekasimov2012@gmail.com>
2023-09-28 14:50:52 +0300

    created TagController, added into GetMapping "users/" endpoint

---
+++ src/main/java/example/myVocabulary/controller/TagController.java
@@ -0,0 +1,33 @@
+package example.myVocabulary.controller;
+
+import example.myVocabulary.dto.TagTransformer;
+import example.myVocabulary.dto.WordTransformer;
+import example.myVocabulary.service.TagService;
+import example.myVocabulary.service.WordService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/tags")
+@RequiredArgsConstructor
+public class TagController {
+    private final TagService tagService;
+    private final TagTransformer tagTransformer;
+    private final WordService wordService;
+    private final WordTransformer wordTransformer;
+
+    @GetMapping(value = {"/"})
+    public String getHome(Model model) {
+        model.addAttribute("tags",
+                tagService.getAll().stream()
+                        .map(tagTransformer::fromEntity)
+                        .collect(Collectors.toList()));
+        return "tag/tag-list";
+    }
+
+}
