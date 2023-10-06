efa34f25aca68442fa8ed5dcf47aa8013a24b016
ElnurKasimov <ekasimov2012@gmail.com>
2023-10-06 23:47:53 +0300

    handled endpoint 'words/create' but it doesn't work

--- src/main/java/example/myVocabulary/controller/WordController.java
+++ src/main/java/example/myVocabulary/controller/WordController.java
@@ -1,17 +1,22 @@
 package example.myVocabulary.controller;
 
+import example.myVocabulary.dto.TagResponse;
 import example.myVocabulary.dto.TagTransformer;
+import example.myVocabulary.dto.WordRequest;
 import example.myVocabulary.dto.WordTransformer;
+import example.myVocabulary.model.Tag;
+import example.myVocabulary.model.Word;
 import example.myVocabulary.service.TagService;
 import example.myVocabulary.service.WordService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
@@ -48,6 +53,42 @@ public class WordController {
         model.addAttribute("word", wordPart);
         model.addAttribute("searchPerformed", searchPerformed);
         return "word/find";
+    }
+
+    @GetMapping(value = {"/create"})
+    public String getCreatedWord(
+//            @ModelAttribute("tag") Object tag,
+//            @ModelAttribute("words") Object words,
+                                 Model model) {
+        List<TagResponse> tags = tagService.getAll().stream()
+                        .map(tagTransformer::fromEntity)
+                        .toList();
+        model.addAttribute("tags",tags);
+        model.addAttribute("wordRequest", new WordRequest());
+        return "word/create";
+    }
+    @PostMapping(value = {"/create"})
+    public String postCreateWord(@ModelAttribute("wordRequest") @Valid WordRequest wordRequest,
+                                 BindingResult bindingResult,
+                                 Model model
+//                                 RedirectAttributes redirectAttributes
+    ) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            List<TagResponse> tags = tagService.getAll().stream()
+                    .map(tagTransformer::fromEntity)
+                    .toList();
+            model.addAttribute("tags",tags);
+            return "word/create";
+        }
+        Word newWord = wordService.create(wordTransformer.toEntity(wordRequest));
+        Tag tag = tagService.readByName(wordRequest.getTagName());
+//        redirectAttributes.addFlashAttribute("tag", tag);
+//        redirectAttributes.addFlashAttribute("words", tag.getWords());
+        model.addAttribute("tag", tag);
+        model.addAttribute("words",tag.getWords());
+        return "word/create";
     }
 
 }
