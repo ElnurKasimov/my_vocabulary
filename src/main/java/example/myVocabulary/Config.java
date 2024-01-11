463372e3ee49cc0622c1e5a660188ba7b4408657
Elnur Kasimov <ekasimov2012@gmail.com>
2024-01-11 17:03:42 +0200

    added to Config @Preconstruct wwirth checking if a BD exist

---
+++ src/main/java/example/myVocabulary/Config.java
@@ -0,0 +1,38 @@
+package example.myVocabulary;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+import javax.sql.DataSource;
+import java.io.File;
+
+@Configuration
+@RequiredArgsConstructor
+public class Config implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+    private final DataSource dataSource;
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        factory.addErrorPages(
+                new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
+    }
+
+    @PostConstruct
+    public void migrateDatabase() {
+        if (!databaseFileExists()) {
+            Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+            flyway.migrate();
+        }
+    }
+
+    private boolean databaseFileExists() {
+        File databaseFile = new File("./database.mv.db");
+        return databaseFile.exists();
+    }
+}
