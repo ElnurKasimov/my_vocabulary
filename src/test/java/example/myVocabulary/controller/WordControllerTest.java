730fc24f3d1dcf0db23e4bd2ba5b271a7b895fdc
ElnurKasimov <ekasimov2012@gmail.com>
2024-01-20 22:20:04 +0200

    created stub for WordControllerTest

---
+++ src/test/java/example/myVocabulary/controller/WordControllerTest.java
@@ -0,0 +1,40 @@
+package example.myVocabulary.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordControllerTest {
+
+    @Test
+    void getAllWords() {
+    }
+
+    @Test
+    void getFindWord() {
+    }
+
+    @Test
+    void postFindWord() {
+    }
+
+    @Test
+    void getCreatedWord() {
+    }
+
+    @Test
+    void postCreateWord() {
+    }
+
+    @Test
+    void getDeleteWord() {
+    }
+
+    @Test
+    void getUpdateWord() {
+    }
+
+    @Test
+    void postUpdateWord() {
+    }
+}
