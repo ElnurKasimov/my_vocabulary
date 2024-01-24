c092248ea2f58b09c661bd7f823333246979da1b
Elnur Kasimov <ekasimov2012@gmail.com>
2024-01-24 18:10:13 +0200

    model Tag has been covered by validation test

---
+++ src/test/java/example/myVocabulary/model/TagTest.java
@@ -0,0 +1,65 @@
+package example.myVocabulary.model;
+
+
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagTest {
+
+    @Test
+    @DisplayName("Test test that field 'name' of the Tag not empty")
+    void testThatNameNotEmpty() {
+        Tag tag = new Tag();
+        tag.setName("");
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Tag>> violations = validator.validate(tag);
+        assertEquals(1, violations.size());
+        assertEquals("", violations.iterator().next().getInvalidValue());
+        factory.close();
+    }
+
+    @Test
+    @DisplayName("Test test that field 'name' of the Tag not blank")
+    void ctestThatNameNotBlank() {
+        Tag tag = new Tag();
+        tag.setName("   ");
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Tag>> violations = validator.validate(tag);
+        assertEquals(1, violations.size());
+        assertEquals("   ", violations.iterator().next().getInvalidValue());
+        factory.close();
+    }
+
+    @Test
+    @DisplayName("Test test that field 'name' of the Tag not null")
+    void ctestThatNameNotNull() {
+        Tag tag = new Tag();
+        tag.setName(null);
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Tag>> violations = validator.validate(tag);
+        assertEquals(2, violations.size());
+        assertEquals(null, violations.iterator().next().getInvalidValue());
+        factory.close();
+    }
+
+}
