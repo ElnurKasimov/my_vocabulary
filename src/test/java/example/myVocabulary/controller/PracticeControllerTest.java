6da0529f860bb0a5c9e701d410b6b722f04f2519
ElnurKasimov <ekasimov2012@gmail.com>
2024-03-01 21:11:17 +0200

    prepared stubs for all methods of PracticeControllerTest

---
+++ src/test/java/example/myVocabulary/controller/PracticeControllerTest.java
@@ -0,0 +1,24 @@
+package example.myVocabulary.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PracticeControllerTest {
+
+    @Test
+    void getInfoForPractice() {
+    }
+
+    @Test
+    void postInfoForPractice() {
+    }
+
+    @Test
+    void getTagForPractice() {
+    }
+
+    @Test
+    void postTagForPractice() {
+    }
+}
